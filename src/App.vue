127acede533cd258ae72993f1702aca47a785acd
odangosan <odangosan333@gmail.com>
2019-04-30 21:39:31 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>SPLATOON2 UTILITY TOOL</span>
-        <v-btn outline color="primary" dark @click="addUser()">add</v-btn>
-        <v-btn outline color="primary" dark>desc</v-btn>
       </v-toolbar-title>
       <v-divider class="mx-2" inset vertical></v-divider>
       <v-spacer></v-spacer>
@@ -13,141 +11,26 @@
     <v-content>
       <v-tabs dark color="cyan" show-arrows>
         <v-tabs-slider color="red"></v-tabs-slider>
-
         <v-tab :href="'#tab-main'">main</v-tab>
-        <v-tab v-for="i in selected" :key="i.name" :href="'#tab-' + i.name">{{ i.name }}</v-tab>
+        <!-- <v-tab v-for="i in selected" :key="i.name" :href="'#tab-' + i.name">{{ i.name }}</v-tab> -->
 
         <v-tabs-items>
           <v-tab-item :value="'tab-main'">
-            <!-- <hello-world></hello-world> -->
-            <div>
-              <v-toolbar flat color="white">
-                <v-text-field v-model="search" append-icon="search" label="Search" hide-details></v-text-field>
-                <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="500px">
-                  <template v-slot:activator="{ on }">
-                    <v-btn color="primary" dark class="mb-2" v-on="on">新しいプレイヤーを登録する</v-btn>
-                  </template>
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline">{{ formTitle }}</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container grid-list-md>
-                        <v-layout wrap>
-                          <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                          </v-flex>
-                          <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                          </v-flex>
-                          <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                          </v-flex>
-                          <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                          </v-flex>
-                          <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                          </v-flex>
-                        </v-layout>
-                      </v-container>
-                    </v-card-text>
-
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-                      <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-toolbar>
-              <v-data-table
-                v-model="selected"
-                :headers="headers"
-                :items="desserts"
-                class="elevation-1"
-                select-all
-                item-key="name"
-                :rows-per-page-items="rowsPerPageItems"
-                :pagination.sync="pagination"
-                :search="search"
-              >
-                <template v-slot:items="props">
-                  <td>
-                    <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
-                  </td>
-                  <!-- <td
-                  v-for="header in headers"
-                  class="text-xs-right"
-                  :key="header.name"
-                  >{{ props.item[header.value] }}</td>-->
-                  <td>{{ props.item.name }}</td>
-                  <td class="text-xs-right">{{ props.item.calories }}</td>
-                  <td class="text-xs-right">{{ props.item.fat }}</td>
-                  <td class="text-xs-right">{{ props.item.carbs }}</td>
-                  <td class="text-xs-right">{{ props.item.protein }}</td>
-                  <td class="justify-center layout px-0">
-                    <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
-                    <v-icon small @click="deleteItem(props.item)">delete</v-icon>
-                  </td>
-                </template>
-                <template v-slot:no-data>
-                  <v-btn color="primary" @click="initialize">Reset</v-btn>
-                </template>
-              </v-data-table>
-              <!-- <v-card flat>
-              <v-card-text>{{ text }}{{i}}</v-card-text>
-              <v-btn @click="add()">add</v-btn>
-              </v-card>-->
-              <!-- <div>{{selected}}</div>
-              <div>{{desserts}}</div>-->
-              <span>{{users}}</span>
-            </div>
+            <main-tab></main-tab>
           </v-tab-item>
-          <v-tab-item v-for="i in selected" :key="i.name" :value="'tab-' + i.name">
-            <v-data-table
-              v-model="selected"
-              :headers="headers"
-              :items="desserts"
-              class="elevation-1"
-              select-all
-              item-key="name"
-              :search="search"
-            >
-              <template v-slot:items="props">
-                <td>
-                  <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
-                </td>
-                <td>{{ props.item.name }}</td>
-                <td class="text-xs-right">{{ props.item.calories }}</td>
-                <td class="text-xs-right">{{ props.item.fat }}</td>
-                <td class="text-xs-right">{{ props.item.carbs }}</td>
-                <td class="text-xs-right">{{ props.item.protein }}</td>
-                <td class="justify-center layout px-0">
-                  <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
-                  <v-icon small @click="deleteItem(props.item)">delete</v-icon>
-                </td>
-              </template>
-              <template v-slot:no-data>
-                <v-btn color="primary" @click="initialize">Reset</v-btn>
-              </template>
-            </v-data-table>
-            <!-- <v-card flat>
-              <v-card-text>{{ text }}{{i}}</v-card-text>
-              <v-btn @click="add()">add</v-btn>
-            </v-card>-->
+          <!-- <v-tab-item v-for="i in selected" :key="i.name" :value="'tab-' + i.name">
             <div>{{selected}}</div>
-          </v-tab-item>
+          </v-tab-item>-->
         </v-tabs-items>
       </v-tabs>
     </v-content>
   </v-app>
 </template>
 
-<script>
+ <script>
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld";
+import MainTab from "@/components/MainTab";
+import { DataTableHeader } from "@/models/Defs";
 import { CounterModule } from "@/store/modules/Counter";
 import { ConstantModule } from "@/store/modules/Constant";
 import {
@@ -159,238 +42,12 @@ import {
 
 export default Vue.extend({
   data() {
-    return {
-      pagination: {
-        rowsPerPage: 5
-      },
-      rowsPerPageItems: [
-        5,
-        10,
-        15,
-        25,
-        { text: "$vuetify.dataIterator.rowsPerPageAll", value: -1 }
-      ],
-      search: "",
-      selected: [],
-      tabs: ["1", "2", "3"],
-      text: "test",
-      dialog: false,
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "left",
-          sortable: false,
-          value: "name"
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Actions", value: "name", sortable: false }
-      ],
-      desserts: [],
-      editedIndex: -1,
-      editedItem: {
-        name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      },
-      defaultItem: {
-        name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      }
-    };
-  },
-  methods: {
-    addUser() {
-      StorableModule.addUser(new User({ name: "test" }));
-    },
-    desc() {},
-    destroyed() {
-      StorableModule.save();
-    },
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
-        }
-      ];
-      this.selected = [
-        {
-          name: "Frozen Yogurt",
-          calories: 1579,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        }
-      ];
-    },
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-    deleteItem(item) {
-      const index = this.desserts.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.desserts.splice(index, 1);
-    },
-    close() {
-      this.dialog = false;
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      }, 300);
-    },
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-      }
-      this.close();
-    }
-  },
-  watch: {
-    dialog(newValue, oldValue) {
-      this.dialog || this.close();
-    },
-    storeCount(newValue, oldValue) {
-      console.log("aaaa");
-      StorableModule.save();
-    },
-    deep: true
-  },
-  computed: {
-    storeCount() {
-      return StorableModule.KEY;
-    },
-    etabs() {
-      var t = [];
-      this.tabs.forEach(e => {
-        t.push(e + "tab");
-      });
-      return t;
-    },
-    formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
-    },
-    dessertFiltered() {
-      return this.desserts.filter(e => {
-        return e.calories > 300;
-      });
-    },
-    users() {
-      return StorableModule.StoredObject.users;
-    }
+    return {};
   },
   created() {
     StorableModule.load();
     ConstantModule.load();
-    this.initialize();
   },
-  components: {
-    HelloWorld
-  }
+  components: { MainTab }
 });
 </script>
